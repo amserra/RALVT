71fd0b75d41e0626546cb91d789cbe0dab9a3038
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-03-23 17:01:34 +0000

    Apply prettier to existing files

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,18 +2,18 @@
 // for information about these interfaces
 // and what to do when importing types
 
-import type { TypedSupabaseClient } from "@supabase/auth-helpers-sveltekit/dist/types"
-import type { Session } from "@supabase/supabase-js"
+import type { TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit/dist/types';
+import type { Session } from '@supabase/supabase-js';
 
 declare global {
 	declare namespace App {
 		// interface Error {}
 		interface Locals {
-			sb: TypedSupabaseClient
-			session: Session | null
+			sb: TypedSupabaseClient;
+			session: Session | null;
 		}
 		interface PageData {
-			session: import("@supabase/supabase-js").Session | null
+			session: import('@supabase/supabase-js').Session | null;
 		}
 		// interface Platform {}
 	}
