8b47c490ac9b5b8deb41cc23e65b3313ee2f7d6e
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-03-23 16:52:01 +0000

    Use auth boilerplate

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { supabaseClient } from '$lib/supabase';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidateAll();
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
+</script>
+
+<slot />
