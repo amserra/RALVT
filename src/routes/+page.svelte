6041d771f77cd8207689f0849285ab37cc4dab75
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-04-28 19:16:45 +0100

    Home page with video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
 <main>
-	<h1>SvelteKit</h1>
+	<div class="h-screen relative">
+		<video autoplay muted loop class="h-full object-fill grayscale-[40%]">
+			<source
+				src="https://assets.mixkit.co/videos/preview/mixkit-dolphins-underwater-4133-large.mp4"
+			/>
+		</video>
+		<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+			<h1 class="text-center text-white text-8xl font-bold">RALVT</h1>
+			<h2 class="text-center text-white text-4xl mt-6">
+				Rede de Arrojamentos de Lisboa e Vale do Tejo
+			</h2>
+		</div>
+	</div>
 </main>
